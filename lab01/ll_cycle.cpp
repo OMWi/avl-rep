13eea75e913dff0afeaa4e21f5f5a36bb2f62a6c
OMWi <bsuiromwi@gmail.com>
2021-03-28 23:17:58 +0300

    task5

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,17 @@
 #include "ll_cycle.h"
 
 bool ll_has_cycle(node *head) {
-    /* Ваш код должен быть написан только внутри этой функции */
-    return true;
+    node* turtle = head;
+    node* rabbit = head;
+    while(true)
+    {
+        if(!rabbit ||!(rabbit->next)) return false;
+        rabbit = rabbit->next->next;
+        turtle = turtle->next;
+        if(rabbit == turtle)
+        {
+            return true;
+        }
+    }
+    return false;
 }
